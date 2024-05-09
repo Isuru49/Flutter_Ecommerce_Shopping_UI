3f7daae462c64520d0511a111d379366a7bbc0cc
Isuru49 <114619691+Isuru49@users.noreply.github.com>
2024-05-09 11:33:24 +0530

    updated

--- flutter_ecommerce_shopping_ui/lib/screen/splash_screen.dart
+++ flutter_ecommerce_shopping_ui/lib/screen/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_shopping_ui/screen/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,13 +12,46 @@ class SplashScreen extends StatefulWidget {
 
 class _MyWidgetState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+    ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen(), ))
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        children: [
-          Image.asset("images/image2.jpg"),
-        ],
-      ),
+      child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            color: Colors.black,
+            image: DecorationImage(
+              image: AssetImage("images/image2.jpg"),
+              fit: BoxFit.cover,
+              opacity: 0.4,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.shopping_cart,
+                size: 250,
+                color: Color.fromARGB(255, 239, 48, 48),
+              ),
+              Text(
+                "DP Shop",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
