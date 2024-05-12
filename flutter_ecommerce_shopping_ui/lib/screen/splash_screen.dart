af231f064b6e1fcf019c038e48391ceab0ced945
Isuru49 <114619691+Isuru49@users.noreply.github.com>
2024-05-12 18:01:27 +0530

    updated

--- flutter_ecommerce_shopping_ui/lib/screen/splash_screen.dart
+++ flutter_ecommerce_shopping_ui/lib/screen/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_shopping_ui/screen/home_screen.dart';
+import 'package:flutter_ecommerce_shopping_ui/screen/onboarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -15,7 +16,7 @@ class _MyWidgetState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(Duration(seconds: 3),
-    ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen(), ))
+    ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => OnboardingScreen(), ))
     );
   }
 
