9b87a29ab862099dc0e85f1b14297fece6625f78
Isuru49 <114619691+Isuru49@users.noreply.github.com>
2024-05-12 19:54:54 +0530

    update

--- flutter_ecommerce_shopping_ui/lib/screen/splash_screen.dart
+++ flutter_ecommerce_shopping_ui/lib/screen/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce_shopping_ui/screen/home_screen.dart';
+//import 'package:flutter_ecommerce_shopping_ui/screen/home_screen.dart';
 import 'package:flutter_ecommerce_shopping_ui/screen/onboarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
